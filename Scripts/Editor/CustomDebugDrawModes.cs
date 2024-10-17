3d95c15fa148c1a28ccda23f559b77311b182e5f
Roy Theunissen <roy.theunissen@live.nl>
2024-10-17 22:23:28 +0200

    Added some graceful error handling on ill configured projects.

--- Scripts/Editor/CustomDebugDrawModes.cs
+++ Scripts/Editor/CustomDebugDrawModes.cs
@@ -1,5 +1,8 @@
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace RoyTheunissen.SceneViewDebugModes
 {
@@ -12,17 +15,50 @@ namespace RoyTheunissen.SceneViewDebugModes
         static CustomDebugDrawModes()
         {
             EditorApplication.update += OnUpdateEditor;
-
+            
             if (CustomDebugDrawModesConfig.Instance == null)
             {
                 Debug.LogError($"Custom Debug Draw Modes Config could not be found. Debug draw modes will not " +
-                                 $"work. If this is your first time installing it, it may not have loaded correctly " +
-                                 $"in which case it should work after a restart. If it continues to persist, check " +
-                                 $"that a {nameof(CustomDebugDrawModesConfig)} exists with GUID " +
-                                 $"{CustomDebugDrawModesConfig.CustomDebugDrawModesConfigGuid}.");
+                               $"work. If this is your first time installing it, it may not have loaded correctly " +
+                               $"in which case it should work after a restart. If it continues to persist, check " +
+                               $"that a {nameof(CustomDebugDrawModesConfig)} exists with GUID " +
+                               $"{CustomDebugDrawModesConfig.CustomDebugDrawModesConfigGuid}.");
+                return;
+            }
+
+            EditorApplication.delayCall += InitializeAfterDelay;
+        }
+
+        private static void InitializeAfterDelay()
+        {
+            UniversalRenderPipelineAsset universalRenderPipelineAsset =
+                GraphicsSettings.currentRenderPipeline as UniversalRenderPipelineAsset;
+            if (universalRenderPipelineAsset == null)
+            {
+                Debug.LogError($"Checked the Default Render Pipeline in the Graphics settings but it was not a " +
+                               $"UniversalRenderPipelineAsset. Did you install URP Buffer Debugging in a BRP or HDRP " +
+                               $"project?");
                 return;
             }
             
+            UniversalRendererData data = universalRenderPipelineAsset.rendererDataList[0] as UniversalRendererData;
+            if (universalRenderPipelineAsset == null)
+            {
+                Debug.LogError($"Checked the Default Render Pipeline's renderers but could not find a " +
+                               $"UniversalRendererData. Did you install URP Buffer Debugging in a BRP or HDRP " +
+                               $"project?");
+                return;
+            }
+            
+            if (data.renderingMode != RenderingMode.Deferred)
+            {
+                Debug.LogError($"Custom Debug Draw Modes will not be registered because it seems like your project " +
+                               $"is set to {data.renderingMode}, and the Debug Draw Modes are for " +
+                               $"Deferred Rendering URP projects (there seems to currently be no support for " +
+                               $"enabling/disabling custom draw modes so I have to not register them instead)");
+                return;
+            }
+
             CustomDebugDrawModesConfig.Instance.RegisterDebugDrawModes();
         }
 
